6f2044e8610b0bb57a73dd26346e2e6b6ea85cd0
Charles Ouimet <charles.ouimet@gmail.com>
2026-07-14 15:53:47 -0400

    More fixes

--- dashboard/src/components/usePauseNotification.ts
+++ dashboard/src/components/usePauseNotification.ts
@@ -1,4 +1,4 @@
-import { MS_PER_MINUTE } from '../../../src/utils/durations.js';
+import { MS_PER_SECOND } from '../../../src/utils/durations.js';
 import { fetchConfig } from '../api.js';
 import { diagLog } from '../utils/diagLog.js';
 
@@ -62,16 +62,16 @@ export const usePauseNotification = ({ paused }: UsePauseNotificationOptions): v
           return;
         }
 
-        const initialDelayMs = config.pauseNotificationInitialDelaySec * MS_PER_MINUTE;
-        const repeatIntervalMs = config.pauseNotificationRepeatIntervalSec * MS_PER_MINUTE;
+        const initialDelayMs = config.pauseNotificationInitialDelaySec * MS_PER_SECOND;
+        const repeatIntervalMs = config.pauseNotificationRepeatIntervalSec * MS_PER_SECOND;
 
         diagLog(
           'pause-notif',
           'scheduling initial notification in',
           config.pauseNotificationInitialDelaySec,
-          'min, repeat every',
+          'sec, repeat every',
           config.pauseNotificationRepeatIntervalSec,
-          'min',
+          'sec',
         );
 
         initialTimerRef.current = setTimeout(() => {
